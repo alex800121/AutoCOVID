72b2d24d02e5aed7867e1b94fb81eda9407be268
alex800121 <alex800121@hotmail.com>
2022-11-18 16:19:01 +0800

    20221118

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,26 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import MyLib
+import Web.Api.WebDriver
+import Web.Api.WebDriver.Monad
+import System.Process
+import Control.Monad.IO.Class
+
+defaultChromeConfig = defaultWebDriverConfig {
+  _environment = defaultWebDriverEnvironment {
+    _env = defaultWDEnv {
+      _remotePort = 9515
+    }
+  }
+}
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  -- r <- createProcess $ proc "java" ["-jar", "selenium-server-4.5.3.jar", "standalone"]
+  (_, _, _, r) <- createProcess $ proc "chromedriver.exe" ["--silent"]
+  execWebDriverT defaultChromeConfig $ do
+    runIsolated_ defaultChromeCapabilities $ do
+      loginWith =<< liftIO getUserPass
+      goSIDAS ""
+  terminateProcess r
+  return ()
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,43 @@
-module MyLib (someFunc) where
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use void" #-}
+module MyLib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Text (Text, pack)
+import Web.Api.WebDriver
+import Control.Monad (void)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+
+getUserPass :: IO (Text, Text)
+getUserPass = do
+  username : passwd : _ <- map pack . lines <$> readFile ".\\user.txt"
+  return (username, passwd)
+
+type WDIO a = WebDriverT IO a
+
+loginWith :: (Text, Text) -> WDIO ()
+loginWith (username, passwd) =do
+  navigateTo "https://eip.vghtpe.gov.tw"
+  usernameField <- findElement CssSelector "#login_name"
+  passwdField <- findElement CssSelector "#password"
+  elementSendKeys username usernameField
+  elementSendKeys passwd passwdField
+  elementClick =<< findElement CssSelector "#loginBtn"
+  wait 5000000
+  void $ findElement CssSelector "#fixed_top"
+
+goSIDAS :: Text -> WDIO ()
+goSIDAS patID = do
+  navigateTo "https://web9.vghtpe.gov.tw/nicc/servlet/sidas.Controller?event=Forward&action=First"
+  -- switchToFrame . FrameContainingElement =<< findElement XPathSelector "/html/body/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/a"
+  -- elementClick =<< findElement XPathSelector "/html/body/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/a"
+  navigateTo "https://web9.vghtpe.gov.tw/nicc/servlet/sidas.Controller?event=Forward&action=Sidastab_New"
+  wait 10000000
+
+doABarrelRoll :: WebDriverT IO ()
+doABarrelRoll = do
+  fullscreenWindow
+  navigateTo "https://www.google.com"
+  performActions [typeString "do a barrel roll"]
+  performActions [press EnterKey]
+  wait 5000000
+  return ()
